c296b02fda89a041d33c2efd8723db55dcc2915c
akaminski <akaminski@polarisio.com>
2023-10-09 01:38:08 -0500

    y

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-var http = require('http');
-var url = require('url');
-var dt = require('./datetime');
+const http = require('http');
+const url = require('url');
+const dt = require('./datetime');
+const express = require('express');
+const cors = require('cors'); // Add the CORS middleware
 
+const app = express();
+const port = process.env.PORT || 1337;
 
-const server = http.createServer((request, response) => {
-    // Write the request to the log. 
-    console.log(request);
+app.use(cors()); // Enable CORS for all routes
 
-    // Standard Hello World.
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h3>Hello World!</h3>')
-
-    // Access funcion from a separate JavaScript module.
-    response.write("The date and time are currently: " + dt.myDateTime() + "<br><br>");
-
-    // Show the url. 
-    response.write("req.url="+request.url+"<br><br>");
-
-    // Suggest adding something tl the url so that we can parse it. 
-    response.write("Consider adding '/test?year=2017&month=July' to the URL.<br><br>");
-    var q = url.parse(request.url, true).query;
-    var txt = q.year + " " + q.month;
-    response.write("txt="+txt);
-
-    // Close the response
-    response.end('<h3>The End.</h3>');
+// Define your RESTful API routes here
+app.get('/api/rollDice', (req, res) => {
+    // Generate a random number between 1 and 6
+    const randomValue = Math.floor(Math.random() * 6) + 1;
+    
+    // Respond with the random value
+    res.json({ diceValue: randomValue });
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+app.get('/', (req, res) => {
+    // Standard Hello World.
+    res.send('<h3>Hello World!</h3>');
+});
 
-console.log("Server running at http://localhost:%d", port);
+app.listen(port, () => {
+    console.log("Server running at http://localhost:%d", port);
+});
