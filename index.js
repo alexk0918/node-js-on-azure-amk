11a975beb1703d72cf2316c46c058ebdb80b7b98
alexk0918 <95840883+alexk0918@users.noreply.github.com>
2023-10-09 01:53:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const url = require('url');
+const dt = require('./datetime');
 const express = require('express');
 const cors = require('cors'); // Add the CORS middleware
 
@@ -7,13 +9,18 @@ const port = process.env.PORT || 1337;
 
 app.use(cors()); // Enable CORS for all routes
 
-// Define your RESTful API route for rolling the dice
-app.get('/rollDice', (req, res) => {
+// RESTful API routes here
+app.get('/api/rollDice', (req, res) => {
     // Generate a random number between 1 and 6
     const randomValue = Math.floor(Math.random() * 6) + 1;
-
-    // Respond with the random value as JSON
+    
+    // Respond with the random value
     res.json({ diceValue: randomValue });
+});
+
+app.get('/', (req, res) => {
+    // Hello World.
+    res.send('<h3>Hello World!</h3>');
 });
 
 app.listen(port, () => {
